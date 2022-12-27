3e6d2a8a5c30cf853dd484bad69bdcd5d02d0df8
v38armageddon <69727502+v38armageddon@users.noreply.github.com>
2022-12-27 12:57:40 +0100

    Finish Scan-IpAddress

--- src/Pwsh-IpScanner.psm1
+++ src/Pwsh-IpScanner.psm1
@@ -1,17 +1,24 @@
 function Scan-IpAddress {
     <#
     .SYNOPSIS
-        Scan a specific IP Address.
+    Scan a specific IP Address.
+    
     .DESCRIPTION
-        You can scan a specific IP Address and even with a specific port number. It allow you to determine if X IP (and X port number) is pingable or not.
-    .PARAMETER
-        -IpAddress
-            Specifies the IP address.
-        -Port
-            Specifies the port number.
+    You can scan a specific IP Address and even with a specific port number. It allow you to determine if X IP (and X port number) is pingable or not.
+    
+    .PARAMETER IpAddress
+    Specifies the IP address.
+    
+    .PARAMETER Port
+    Specifies the port number.
+    
     .EXAMPLE
-        Scan-IpAddress -IpAddress 1.1.1.1
-        Scan-IpAddress -IpAddress 192.168.1.2 - Port 25565
+    Scan-IpAddress -IpAddress 1.1.1.1
+    .EXAMPLE
+    Scan-IpAddress -IpAddress 192.168.1.2 - Port 25565
+    
+    .NOTES
+    A file named "report-ipscan.txt" (and "report-portscan.txt" if -Port specified) is generated when the command is finished to be executed.
     #>
 
     [cmdletbinding()]
@@ -22,17 +29,52 @@ function Scan-IpAddress {
         [String]$Port
     )
 
-    if ($PSBoundParameters.ContainsKey('Port')) {
-        Test-Connection -IPv4 $IpAddress -Ping -Quiet | Out-File report-ipscan.txt
-        Test-Connection -IPv4 $IpAddress -TcpPort $Port | Out-File report-portscan.txt
+    $ipResult = Test-Connection -IPv4 $IpAddress -Ping -Quiet
+    if ($ipResult) {
+        "IP: $IpAddress | Status: Up" | Out-File .\report-ipscan.txt -Append
+    } else {
+        "IP: $IpAddress | Status: Down" | Out-File .\report-ipscan.txt -Append
     }
-    else {
-        Test-Connection -IPv4 $IpAddress -Ping
-        Out-File report-scan.txt
-    }   
+
+    if ($PSBoundParameters.ContainsKey('Port')) {
+        $portResult = Test-Connection -IPv4 $IpAddress -TcpPort $Port -Quiet
+        if ($portResult) {
+            "IP: $IpAddress | Port: $Port | Status: Open" | Out-File .\report-portscan.txt -Append
+        } else {
+            "IP: $IpAddress | Port: $Port | Status: Closed" | Out-File .\report-portscan.txt -Append
+        }
+    }
 }
 
 function Scan-IpRange {
+    <#
+    .SYNOPSIS
+    Scan a range of IP addresses.
+    
+    .DESCRIPTION
+    You can scan a range of IP addresses whenever it's CIDR and even specific a range of port numbers.
+    
+    .PARAMETER StartIP
+    Give the first IP address to be scanned.
+    
+    .PARAMETER EndIP
+    Give the last IP address to be scanned.
+    
+    .PARAMETER StartPort
+    Give the first port to be scanned.
+    
+    .PARAMETER EndPort
+    Give the last port to be scanned.
+
+    .EXAMPLE
+    Scan-IpRange -StartIP 192.168.1.1 -EndIP 192.168.1.254
+    .EXAMPLE
+    Scan-IpRange -StartIP 192.168.1.1 -EndIP 192.168.1.254 -StartPort 1 -EndPort 65535
+    
+    .NOTES
+    A file named "report-ipscan.txt" (and "report-portscan.txt" if ports are specified) is generated when the command is finished to be executed.
+    #>
+
     [CmdletBinding()]
     param (
         [Parameter(Mandatory=$true)]
@@ -41,21 +83,12 @@ function Scan-IpRange {
         [Parameter(Mandatory=$true)]
         [string]$EndIP,
 
-        [int]$Port = 80
+        [Parameter(Mandatory=$false)]
+        [string]$StartPort,
+
+        [Parameter(Mandatory=$false)]
+        [string]$EndPort
     )
 
-    $network = $IpAddress
-    $range = (1..254)
-    $reachableIpAddresses = @()
-
-    $ErrorActionPreference = 'silentlycontinue'
-
-    foreach ($add in $range) {
-        $ipAddress = "$network.$add"
-        if (Test-NetConnection -RemoteAddress $ipAddress -Port $port -ErrorAction SilentlyContinue) {
-            $reachableIpAddresses += $ipAddress
-        }
-    }
-
-    $reachableIpAddresses | Out-File -FilePath .\report-scan.txt
+    
 }
